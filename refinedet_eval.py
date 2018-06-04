b0a627edd6cfa61207e76746907d08fb45a5629f
david <lexuszhi1990@gmail.com>
2018-06-04 20:26:02 +0800

    rename files

---
+++ refinedet_eval.py
@@ -0,0 +1,187 @@
+# -*- coding: utf-8 -*-
+
+import sys
+import os
+import time
+import numpy as np
+import argparse
+import pickle
+from pathlib import Path
+from collections import OrderedDict
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.backends.cudnn as cudnn
+import torchvision.transforms as transforms
+import torch.nn.init as init
+from torch.autograd import Variable
+import torch.utils.data as data
+
+from src.config import config
+from src.data.data_augment import detection_collate, BaseTransform, preproc
+from src.data.coco import COCODet
+from src.symbol.RefineSSD_vgg import build_net
+from src.loss import RefineMultiBoxLoss
+from src.detection import Detect
+from src.prior_box import PriorBox
+from src.utils import str2bool
+from src.utils.nms_wrapper import nms
+from src.utils.timer import Timer
+
+parser = argparse.ArgumentParser(
+    description='Receptive Field Block Net Training')
+parser.add_argument('-v', '--version', default='Refine_vgg',
+                    help='Refine_vgg')
+parser.add_argument('-s', '--size', default='320',
+                    help='320 or 512 input size.')
+parser.add_argument('-d', '--dataset', default='VOC',
+                    help='VOC or COCO dataset')
+parser.add_argument(
+    '--basenet', default='/mnt/lvmhdd1/zuoxin/ssd_pytorch_models/vgg16_reducedfc.pth', help='pretrained base model')
+#parser.add_argument(
+#    '--basenet', default='/mnt/lvmhdd1/zuoxin/ssd_pytorch_models/mb.pth', help='pretrained base model')
+parser.add_argument('--jaccard_threshold', default=0.5,
+                    type=float, help='Min Jaccard index for matching')
+parser.add_argument('-b', '--batch_size', default=32,
+                    type=int, help='Batch size for training')
+parser.add_argument('--num_workers', default=8,
+                    type=int, help='Number of workers used in dataloading')
+parser.add_argument('--cuda', default=True,
+                    type=bool, help='Use cuda to train model')
+parser.add_argument('--gpu_ids', nargs='+', default=[], help='gpu id')
+parser.add_argument('--lr', '--learning-rate',
+                    default=1e-3, type=float, help='initial learning rate')
+parser.add_argument('--momentum', default=0.9, type=float, help='momentum')
+
+parser.add_argument('--resume_net', default=False, help='resume net for retraining')
+parser.add_argument('--resume_epoch', default=0,
+                    type=int, help='resume iter for retraining')
+
+parser.add_argument('-max','--max_epoch', default=300,
+                    type=int, help='max epoch for retraining')
+parser.add_argument('-we','--warm_epoch', default=1,
+                    type=int, help='max epoch for retraining')
+parser.add_argument('--weight_decay', default=5e-4,
+                    type=float, help='Weight decay for SGD')
+parser.add_argument('--gamma', default=0.1,
+                    type=float, help='Gamma update for SGD')
+parser.add_argument('--log_iters', default=True,
+                    type=bool, help='Print the loss at each iteration')
+parser.add_argument('--save_folder', default='/mnt/lvmhdd1/zuoxin/ssd_pytorch_models/refine/',
+                    help='Location to save checkpoint models')
+parser.add_argument('--date',default='0327')
+parser.add_argument('--save_frequency',default=10)
+parser.add_argument('--retest', default=False, type=bool,
+                    help='test cache results')
+parser.add_argument('--test_frequency',default=10)
+parser.add_argument('--visdom', default=False, type=str2bool, help='Use visdom to for loss visualization')
+parser.add_argument('--send_images_to_visdom', type=str2bool, default=False, help='Sample a random image from each 10th batch, send it to visdom after augmentations step')
+
+
+def val(net, detector, priors, testset, num_classes, transform, save_folder, enable_cuda=False, max_per_image=300, thresh=0.005):
+
+    if not os.path.exists(save_folder):
+        os.mkdir(save_folder)
+
+    # dump predictions and assoc. ground truth to text file for now
+    num_images = len(testset)
+    all_boxes = [[[] for _ in range(num_images)]
+                 for _ in range(num_classes)]
+
+    _t = {'im_detect': Timer(), 'misc': Timer()}
+    det_file = os.path.join(save_folder, 'detections.pkl')
+
+    for i in range(num_images):
+        img = testset.pull_image(i)
+        x = Variable(transform(img).unsqueeze(0), volatile=True)
+        if enable_cuda:
+            x = x.cuda()
+
+        _t['im_detect'].tic()
+        out = net(x=x, test=True)  # forward pass
+        arm_loc, arm_conf, odm_loc, odm_conf = out
+        boxes, scores = detector.forward((odm_loc,odm_conf), priors, (arm_loc,arm_conf))
+        detect_time = _t['im_detect'].toc()
+        boxes = boxes[0]
+        scores=scores[0]
+        boxes = boxes.cpu().numpy()
+        scores = scores.cpu().numpy()
+        # scale each detection back up to the image
+        scale = torch.Tensor([img.shape[1], img.shape[0],
+                              img.shape[1], img.shape[0]]).cpu().numpy()
+        boxes *= scale
+
+        _t['misc'].tic()
+
+        for j in range(1, num_classes):
+            inds = np.where(scores[:, j] > thresh)[0]
+            if len(inds) == 0:
+                all_boxes[j][i] = np.empty([0, 5], dtype=np.float32)
+                continue
+            c_bboxes = boxes[inds]
+            c_scores = scores[inds, j]
+            c_dets = np.hstack((c_bboxes, c_scores[:, np.newaxis])).astype(
+                np.float32, copy=False)
+
+            keep = nms(c_dets, 0.45, force_cpu=True)
+            keep = keep[:50]
+            c_dets = c_dets[keep, :]
+            all_boxes[j][i] = c_dets
+        if max_per_image > 0:
+            image_scores = np.hstack([all_boxes[j][i][:, -1] for j in range(1,num_classes)])
+            if len(image_scores) > max_per_image:
+                image_thresh = np.sort(image_scores)[-max_per_image]
+                for j in range(1, num_classes):
+                    keep = np.where(all_boxes[j][i][:, -1] >= image_thresh)[0]
+                    all_boxes[j][i] = all_boxes[j][i][keep, :]
+
+        nms_time = _t['misc'].toc()
+
+        if i % 20 == 0:
+            print('im_detect: {:d}/{:d} {:.3f}s {:.3f}s'.format(i + 1, num_images, detect_time, nms_time))
+            _t['im_detect'].clear()
+            _t['misc'].clear()
+
+    with open(det_file, 'wb') as f:
+        pickle.dump(all_boxes, f, pickle.HIGHEST_PROTOCOL)
+
+    print('Evaluating detections')
+    testset.evaluate_detections(all_boxes, save_folder)
+
+if __name__ == '__main__':
+
+    args = parser.parse_args()
+    save_folder = os.path.join(args.save_folder, args.version+'_'+args.size, args.date)
+    enable_cuda = args.cuda and torch.cuda.is_available()
+    if enable_cuda:
+        torch.set_default_tensor_type('torch.cuda.FloatTensor')
+        cudnn.benchmark = True
+    num_classes = 81
+    data_shape = 320
+    resume_net_path = './ckpt/Refine_vgg_COCO_epoches_250.pth'
+
+    net = build_net(data_shape, num_classes, use_refine=True)
+    state_dict = torch.load(resume_net_path)
+    new_state_dict = OrderedDict()
+    for k, v in state_dict.items():
+        head = k[:7]
+        if head == 'module.':
+            name = k[7:] # remove `module.`
+        else:
+            name = k
+        new_state_dict[name] = v
+    net.load_state_dict(new_state_dict)
+    print(net)
+
+    module_cfg = config.coco.dimension_320
+    rgb_std = (1,1,1)
+    rgb_means = (104, 117, 123)
+    priorbox = PriorBox(module_cfg)
+    priors = Variable(priorbox.forward(), volatile=True)
+    detector = Detect(num_classes, 0, module_cfg, object_score=0.01)
+    testset = COCODet(config.coco.root_path, config.coco.val_sets, None)
+    val_trainsform = BaseTransform(net.size, rgb_means, rgb_std, (2, 0, 1))
+    import pdb
+    pdb.set_trace()
+    val(net, detector, priors, testset, num_classes, val_trainsform, save_folder, enable_cuda=enable_cuda, max_per_image=300, thresh=0.005)
--- src/utils/build.py
+++ src/utils/build.py
@@ -12,6 +12,11 @@ from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
 
+# run the customize_compiler
+class custom_build_ext(build_ext):
+    def build_extensions(self):
+        customize_compiler_for_nvcc(self.compiler)
+        build_ext.build_extensions(self)
 
 def find_in_path(name, path):
     "Find a file in a search path"
@@ -21,49 +26,6 @@ def find_in_path(name, path):
         if os.path.exists(binpath):
             return os.path.abspath(binpath)
     return None
-
-
-def locate_cuda():
-    """Locate the CUDA environment on the system
-
-    Returns a dict with keys 'home', 'nvcc', 'include', and 'lib64'
-    and values giving the absolute path to each directory.
-
-    Starts by looking for the CUDAHOME env variable. If not found, everything
-    is based on finding 'nvcc' in the PATH.
-    """
-
-    # first check if the CUDAHOME env variable is in use
-    if 'CUDAHOME' in os.environ:
-        home = os.environ['CUDAHOME']
-        nvcc = pjoin(home, 'bin', 'nvcc')
-    else:
-        # otherwise, search the PATH for NVCC
-        default_path = pjoin(os.sep, 'usr', 'local', 'cuda', 'bin')
-        nvcc = find_in_path('nvcc', os.environ['PATH'] + os.pathsep + default_path)
-        if nvcc is None:
-            raise EnvironmentError('The nvcc binary could not be '
-                                   'located in your $PATH. Either add it to your path, or set $CUDAHOME')
-        home = os.path.dirname(os.path.dirname(nvcc))
-
-    cudaconfig = {'home': home, 'nvcc': nvcc,
-                  'include': pjoin(home, 'include'),
-                  'lib64': pjoin(home, 'lib64')}
-    for k, v in cudaconfig.items():
-        if not os.path.exists(v):
-            raise EnvironmentError('The CUDA %s path could not be located in %s' % (k, v))
-
-    return cudaconfig
-
-
-CUDA = locate_cuda()
-
-# Obtain the numpy include directory.  This logic works across numpy versions.
-try:
-    numpy_include = np.get_include()
-except AttributeError:
-    numpy_include = np.get_numpy_include()
-
 
 def customize_compiler_for_nvcc(self):
     """inject deep into distutils to customize how the dispatch
@@ -103,22 +65,68 @@ def customize_compiler_for_nvcc(self):
     # inject our redefined _compile method into the class
     self._compile = _compile
 
+def locate_cuda():
+    """Locate the CUDA environment on the system
 
-# run the customize_compiler
-class custom_build_ext(build_ext):
-    def build_extensions(self):
-        customize_compiler_for_nvcc(self.compiler)
-        build_ext.build_extensions(self)
+    Returns a dict with keys 'home', 'nvcc', 'include', and 'lib64'
+    and values giving the absolute path to each directory.
 
+    Starts by looking for the CUDAHOME env variable. If not found, everything
+    is based on finding 'nvcc' in the PATH.
+    """
+
+    # first check if the CUDAHOME env variable is in use
+    if 'CUDAHOME' in os.environ:
+        home = os.environ['CUDAHOME']
+        nvcc = pjoin(home, 'bin', 'nvcc')
+    else:
+        # otherwise, search the PATH for NVCC
+        default_path = pjoin(os.sep, 'usr', 'local', 'cuda', 'bin')
+        nvcc = find_in_path('nvcc', os.environ['PATH'] + os.pathsep + default_path)
+        if nvcc is None:
+            raise EnvironmentError('The nvcc binary could not be '
+                                   'located in your $PATH. Either add it to your path, or set $CUDAHOME')
+        home = os.path.dirname(os.path.dirname(nvcc))
+
+    cudaconfig = {'home': home, 'nvcc': nvcc,
+                  'include': pjoin(home, 'include'),
+                  'lib64': pjoin(home, 'lib64')}
+    for k, v in cudaconfig.items():
+        if not os.path.exists(v):
+            raise EnvironmentError('The CUDA %s path could not be located in %s' % (k, v))
+
+    return cudaconfig
+
+try:
+    CUDA = locate_cuda()
+except Exception:
+    CUDA = None
+
+# Obtain the numpy include directory.  This logic works across numpy versions.
+try:
+    numpy_include = np.get_include()
+except AttributeError:
+    numpy_include = np.get_numpy_include()
 
 ext_modules = [
     Extension(
         "nms.cpu_nms",
-        ["nms/cpu_nms.pyx"],
+        sources=["nms/cpu_nms.pyx"],
         extra_compile_args={'gcc': ["-Wno-cpp", "-Wno-unused-function"]},
         include_dirs=[numpy_include]
     ),
-    Extension('nms.gpu_nms',
+    Extension(
+        'pycocotools._mask',
+        sources=['pycocotools/maskApi.c', 'pycocotools/_mask.pyx'],
+        include_dirs=[numpy_include, 'pycocotools'],
+        extra_compile_args={
+            'gcc': ['-Wno-cpp', '-Wno-unused-function', '-std=c99']},
+    ),
+]
+
+if CUDA is not None:
+    ext_modules.append(
+        Extension('nms.gpu_nms',
               ['nms/nms_kernel.cu', 'nms/gpu_nms.pyx'],
               library_dirs=[CUDA['lib64']],
               libraries=['cudart'],
@@ -135,15 +143,8 @@ ext_modules = [
                                            '--compiler-options',
                                            "'-fPIC'"]},
               include_dirs=[numpy_include, CUDA['include']]
-              ),
-    Extension(
-        'pycocotools._mask',
-        sources=['pycocotools/maskApi.c', 'pycocotools/_mask.pyx'],
-        include_dirs=[numpy_include, 'pycocotools'],
-        extra_compile_args={
-            'gcc': ['-Wno-cpp', '-Wno-unused-function', '-std=c99']},
-    ),
-]
+        )
+    )
 
 setup(
     name='mot_utils',
