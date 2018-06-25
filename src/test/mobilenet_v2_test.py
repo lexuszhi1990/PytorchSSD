7176188270278cde73213725746b51cb86a3a45e
david <lexuszhi1990@gmail.com>
2018-06-25 20:58:51 +0800

    add test and RefineSSD_mobilenet_v2_1 module

---
+++ src/test/mobilenet_v2_test.py
@@ -0,0 +1,22 @@
+import sys
+sys.path.append('.')
+
+import torch
+from torch.autograd import Variable
+from src.symbol.RefineSSD_mobilenet_v2 import RefineSSDMobileNet
+from src.symbol.RefineSSD_mobilenet_v2_1 import RefineSSDMobileNetV1
+from src.prior_box import PriorBox
+from src.config import config
+
+cfg = config.list['v6']
+
+# 19:6
+# 4:3
+
+model = RefineSSDMobileNetV1(num_classes=81, base_channel_num=128, width_mult=1, use_refine=True)
+model.initialize_weights()
+# v2 = Variable(torch.randn(1, 3, 960, 540), requires_grad=True)
+# v2 = Variable(torch.randn(1, 3, 512, 288), requires_grad=True)
+v2 = Variable(torch.randn(1, 3, 128*4, 128*3), requires_grad=True)
+y = model(v2)
+
---
+++ src/test/prior_box_test.py
@@ -0,0 +1,10 @@
+import sys
+sys.path.append('.')
+
+from src.prior_box import PriorBox
+from src.config import config
+
+cfg = config.list['v5']
+
+priorbox = PriorBox(cfg)
+prior_data = priorbox.forward()
